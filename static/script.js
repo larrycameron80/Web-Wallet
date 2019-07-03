47d319bc36eb089dc001d8f2329b632c8f45ae1e
Filip B <filip@bointsoft.com>
2019-07-03 18:56:33 +0200

    staking

--- static/script.js
+++ static/script.js
@@ -5,19 +5,36 @@ $(document).ready(function(){
 		$(this).parent().addClass("active")
 	   var section = $(".section."+$(this).attr("section"))
 	   $("body > .section").hide()
+
+
+	   $("body").attr("section",$(this).attr("section"))
 	   section.show()
 
 	})
 
+	$("a[href='#send-spend']").click(function(){  
+		$("a[href='#create-tx']").click()
+	})
+
+	$("a[href='#staking']").click(function(){  
+		$("a[href='#delegate-tx']").click()
+	})
+
+
+
 	// txTemplate.json
 	var txTemplate={"type":"auth/StdTx","value":{"msg":[{"type":"cosmos-sdk/MsgSend","value":{"from_address":"","to_address":"","amount":[{"denom":"","amount":""}]}}],"fee":{"amount":[],"gas":"200000"},"signatures":null,"memo":""}}
 
+
+
+
 	// signatureTemplate.json
    var signatureTemplate ={"pub_key": {"type": "tendermint/PubKeySecp256k1","value":""},"signature": ""}
-    
 
-	var newTx = JSON.parse(JSON.stringify(txTemplate))
 
+   var stakingTemplate ={"type": "auth/StdTx","value":{"msg":[{"type":"cosmos-sdk/MsgDelegate","value":{"delegator_address":"spend","validator_address":"spendvaloper","amount":{"denom":"stake","amount":"150000000"}}}],"fee": {"amount": [],"gas": "200000"},"signatures": null,"memo": ""}}
+
+	var newTx =  {} 
 
 	$("#generate-wallet").click(function(){
 		var mnemonic = spendCrypto.generateMnemonic(); 
@@ -30,7 +47,6 @@ $(document).ready(function(){
 		$(".new-wallet.note").show()
 	})
 
-
 	$("#old-mnemonic").on("keyup change",function(){
 		var mnemonic = $("#old-mnemonic").val().trim().replace(/\s{2,}/g, ' ');
 		
@@ -39,10 +55,18 @@ $(document).ready(function(){
 		$("#public-from-mnemonic").val(wallet.keys.public.bech32.string);
 		$("#address-from-mnemonic").val(wallet.address.bech32.string);
 	})
-   $("#continue-tx").click(generateTx)
 
+   $("#continue-tx").click(generateTx)
    $("#continue-tx").click(function(){
    	$("#tx").val($("#new-tx").val())
+   	$(".section.send-spend a[href='#sign']").click()
+   	$("#request-info").click()
+   })
+
+
+   $("#continue-stake").click(generateStakingTx)
+   $("#continue-stake").click(function(){
+   	$("#tx").val($("#stake-tx").val())
    	$(".section.send-spend a[href='#sign']").click()
    	$("#request-info").click()
    })
@@ -51,7 +75,7 @@ $(document).ready(function(){
 	// $(".section.generate-transaction input ,.section.generate-transaction select ").on("change keyup", generateTx)
 	
 	function generateTx(){
-		console.log(123)
+		var newTx = JSON.parse(JSON.stringify(txTemplate)) ;
 		newTx.value.msg[0].value= {
 	       "from_address": 	$("#from-address").val().trim().replace(/\s{1,}/g, ''),
 	       "to_address": 	$("#to-address").val().trim().replace(/\s{1,}/g, ''),
@@ -71,21 +95,44 @@ $(document).ready(function(){
 	    }
 	}
 
+	function generateStakingTx(){
+		var newTx = JSON.parse(JSON.stringify(stakingTemplate)) ;
+
+		// console.log(1231231)
+		newTx.value.msg[0].value= {
+	       "delegator_address": 	$("#from-delegator").val().trim().replace(/\s{1,}/g, ''),
+	       "validator_address": 	$("#to-validator").val().trim().replace(/\s{1,}/g, ''),
+	       "amount":
+	          {
+	             "denom": 	$("#coin-stake").val(),
+	             "amount": 	$("#amount-stake").val()
+	          }
+	       
+	    }
+		console.log(newTx)
+	    if($("#indent").prop("checked")){
+	    	$("#stake-tx").val(JSON.stringify(newTx, null, 2))
+	    	$("#stake-tx").height("410px")
+	    }else{
+	    	$("#stake-tx").val(JSON.stringify(newTx))
+	    	$("#stake-tx").height("95px")
+	    }
+	}
+
 	// resizing, not part from process
 	$("#tx , #mnemonic-for-sign").on("keyup change",function(){
-		    $(this)[0].style.height = "5px";
-		    $(this)[0].style.height = ($(this)[0].scrollHeight)+"px";
+	    $(this)[0].style.height = "5px";
+	    $(this)[0].style.height = ($(this)[0].scrollHeight)+"px";
 
-		    if($(this).parent().is(".half")){
-		    	$(this).parent().next(".half").find("textarea")[0].style.height =  $(this)[0].style.height
-		    }
-
+	    if($(this).parent().is(".half")){
+	    	$(this).parent().next(".half").find("textarea")[0].style.height =  $(this)[0].style.height
+	    }
 	})
 
 	$("#request-info").on("click",function(){
 		var tx = JSON.parse($("#tx").val());
-		var fromAddress = tx.value.msg[0].value.from_address
-		fetchAccountInfo(fromAddress)
+		var fromAddress = typeof(tx.value.msg[0].value.from_address) == "undefined" ? tx.value.msg[0].value.delegator_address :  tx.value.msg[0].value.from_address  ;
+		fetchAccountInfo(fromAddress);
 	})
 
 
@@ -105,19 +152,13 @@ $(document).ready(function(){
 	}
 
 
-	 // $("#mnemonic-for-sign").on("keyup change",function(){
-	 $("#make-signature").on("click",function(){
+	// $("#mnemonic-for-sign").on("keyup change",function(){
+	$("#make-signature").on("click",function(){
 		var mnemonic = $("#mnemonic-for-sign").val().trim().replace(/\s{2,}/g, ' ');
 		var wallet = spendCrypto.getWalletFromSeed(mnemonic);
 		$("#private-for-sign").val(wallet.keys.private.hex)
-		var addressFrom = JSON.parse($("#tx").val()).value.msg[0].value.from_address
 		doSigning()
-
-	 })
-
-
-	 // $("#tx").on("keyup change",doSigning)
-	 // $(" #private-for-sign , #indent-signed , #account-number , #sequence").on("keyup change",doSigning)
+	})
 
 
 	$("#broadcast-transaction").on("click",function(){
@@ -132,17 +173,15 @@ $(document).ready(function(){
 			"data": $("#signed-tx").val()
 		}
 		$.ajax(settings).done(function (response) {
-		    $("#hash").val(response.txhash);
-		    $("#view-tx").attr("href", "http://18.194.28.213:3000/transactions/"+response.txhash);
-
-				$(".section.send-spend a[href='#broadcast']").click()
+		   $("#hash").val(response.txhash);
+		   $("#view-tx").attr("href", "http://18.194.28.213:3000/transactions/"+response.txhash);
+			$(".section.send-spend a[href='#broadcast']").click()
 		})
 	})		
 
-
-
-
-	 function doSigning(){
+	 // $("#tx").on("keyup change",doSigning)
+	 // $(" #private-for-sign , #indent-signed , #account-number , #sequence").on("keyup change",doSigning)
+	function doSigning(){
 		var tx = JSON.parse($("#tx").val());
 
 		tx = tx.value
@@ -157,12 +196,12 @@ $(document).ready(function(){
 		info  = spendCrypto.getAddressFromPrivateKey(privHex)
 		// {publicKey, address} = spendCrypto.getAddressFromPrivateKey(privHex)
 
-		var fromAddress = tx["msgs"][0].value.from_address
+		// var fromAddress = tx["msgs"][0].value.from_address
+		var fromAddress = typeof(tx.msgs[0].value.from_address) == "undefined" ? tx.msgs[0].value.delegator_address :  tx.msgs[0].value.from_address  ;
 
 		if(fromAddress == info.address){
 			var priv = spendCrypto.bufferFromHex(privHex)
 			var signature = spendCrypto.signWithPrivateKey(tx ,priv ).signature.toString("base64") 
-
 			var sigTamplate = JSON.parse(JSON.stringify(signatureTemplate))
 			
 			sigTamplate.signature=signature
@@ -192,14 +231,7 @@ $(document).ready(function(){
 			$("#signed-tx").val("Please insert valid Mnemonic or Private Key")
 		}
 
-	 }
-
-
-	$("#broadcast").click(function(){
-
-	});
-
-
+	}
 
 	if(location.hash.substr(1)==""){
 		window.location="#send-spend"
